c0a61c818ba84dc1725ccdce11f6dafcb71f4cde
Sérgio Tafine <focoensinoonline@gmail.com>
2023-07-03 15:37:02 -0300

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,6 @@
+document.querySelector('#menu-btn').addEventListener('click', function() {
+    //console.log("Menu");
+
+    document.querySelector('#menu-site').classList.toggle("active");
+    document.querySelector('#menu-icon').classList.toggle("active");
+});
